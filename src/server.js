6548f25ba7d8a102277e518350aac82e80397136
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-31 16:46:01 +0300

    add functions getAllContavts_getContactById

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -20,6 +21,49 @@ export const setupServer = () => {
       },
     }),
   );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      message: 'Successfully found contact with id {contactId}!',
+      data: contact,
+    });
+  });
+
+  // app.get('/students/:studentId', async (req, res, next) => {
+  //   const { studentId } = req.params;
+  //   const student = await getStudentById(studentId);
+
+  //   // Відповідь, якщо контакт не знайдено
+  //   if (!student) {
+  //     res.status(404).json({
+  //       message: 'Student not found',
+  //     });
+  //     return;
+  //   }
+
+  //   // Відповідь, якщо контакт знайдено
+  //   res.status(200).json({
+  //     data: student,
+  //   });
+  // });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
