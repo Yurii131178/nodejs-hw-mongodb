4ce0364caf5df5c539c2dd780471d911400eb889
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-05 12:25:40 +0300

    update Eslint.config.js with import rules for .js files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,41 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import pluginImport from 'eslint-plugin-import';
 
 export default [
   pluginJs.configs.recommended,
+
   {
     files: ['src/**/*.js'],
-    languageOptions: { globals: globals.node },
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      import: pluginImport,
+    },
     rules: {
+      // мої правила
       semi: 'error',
       'no-unused-vars': ['error', { args: 'none' }],
       'no-undef': 'error',
+
+      // додано для ESM: вимагає вказувати .js в імпортах
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'always',
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js'],
+        },
+      },
     },
   },
 ];
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {
   getContactByIdController,
   getcontactsController,
-} from '../controllers/contacts';
+} from '../controllers/contacts.js';
 
 const router = Router();
 
