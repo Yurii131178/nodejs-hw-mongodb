f709e58b8d6d7ac916c4b4cdc5db8163614d29b9
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-05 14:47:00 +0300

    add POST /contacts route and controller for creating new contacts

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import {
+  createContactsController,
   getContactByIdController,
   getСontactsController,
 } from '../controllers/contacts.js';
@@ -10,5 +11,7 @@ const router = Router();
 router.get('/contacts', ctrlWrapper(getСontactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactsController));
 
 export default router;
