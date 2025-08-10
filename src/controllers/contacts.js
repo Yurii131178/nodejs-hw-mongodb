4f2f7906a1e4907404087a6c654b399bc4ee186b
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-10 19:54:16 +0300

    implement sorting in controller

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,13 +7,18 @@ import {
   updateContact,
 } from '../services/contacts.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsController = async (req, res) => {
-  const { page, perPage } = parsePaginationParams(req.query);
+  const { page, perPage } = parsePaginationParams(req.query); // pagination
+
+  const { sortBy, sortOrder } = parseSortParams(req.query); // sorting
 
   const contacts = await getAllContacts({
     page,
     perPage,
+    sortBy,
+    sortOrder,
   });
 
   res.json({
