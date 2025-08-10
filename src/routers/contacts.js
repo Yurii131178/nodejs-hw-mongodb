5d80deb41d38530628324be323b45ebffa8e368a
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-10 17:22:04 +0300

    fix the routes by deleting acossinally printed text

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,12 +12,17 @@ import {
   createContactsSchema,
   updateContactsSchema,
 } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
 
 router.post(
   '/contacts',
@@ -27,10 +32,15 @@ router.post(
 
 router.patch(
   '/contacts/:contactId',
+  isValidId,
   validateBody(updateContactsSchema),
   ctrlWrapper(patchContactController),
 );
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default router;
