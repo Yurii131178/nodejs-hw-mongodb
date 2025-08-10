545d4dd81d5a8d023d27741581a3b66d8f036908
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-10 16:25:33 +0300

    add Joi schemas: createContactsSchema and updateContactsSchema

---
+++ src/validation/contacts.js
@@ -0,0 +1,19 @@
+// src/validation/сontacts.js
+
+import Joi from 'joi';
+
+export const createContactsSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().required(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
+});
+
+export const updateContactsSchema = Joi.object({
+  name: Joi.string().min(3).max(20).optional(),
+  phoneNumber: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
+});
