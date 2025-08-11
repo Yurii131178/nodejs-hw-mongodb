4dde8cd6fee7d6cc2e575ab69f7c68d0088552de
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-11 19:05:20 +0300

    fix grammar mistake: use BE favoUrite instead of AE favOrite

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,4 +5,4 @@ export const SORT_ORDER = {
 
 export const CONTACT_TYPES = ['work', 'home', 'personal'];
 
-export const FAVORITE_FILTER_VALUES = ['true', 'false'];
+export const FAVOURITE_FILTER_VALUES = ['true', 'false'];
