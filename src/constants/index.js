dad318aca717b8efcc553dc24ed2c46a691dae50
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-11 19:02:04 +0300

    add contacts for filtering contactType and isFavorite

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,7 @@ export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
+
+export const CONTACT_TYPES = ['work', 'home', 'personal'];
+
+export const FAVORITE_FILTER_VALUES = ['true', 'false'];
