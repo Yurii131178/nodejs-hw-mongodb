1ddc3ce91e631b8f398fbffe3c84fa3e1ab4e1f1
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-13 21:41:00 +0300

    erase comments

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,3 @@
-//Створіть у файлі src/middlewares/errorHandler.js і застосуйте в файлі src/server.js middleware errorHandler, призначений для обробки помилок у вашому Express-сервері. Цей middleware має приймати чотири аргументи. errorHandler у разі виявлення помилки має відправити клієнту відповідь зі статусом 500 та об’єкт з наступними властивостями:
-
 export const errorHandler = (err, req, res, next) => {
   res.status(500).json({
     message: 'Something went wrong',
