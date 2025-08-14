d26cd9071c2108c05c30e1b2a918161c05d24eb7
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-14 23:19:32 +0300

    fix validateBody

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,7 +1,15 @@
 import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
-  // Цей log покаже, чи middleware взагалі запускається
-  console.log('--- validateBody middleware IS RUNNING ---');
-  next(createHttpError(400, 'This is a test message from validateBody.'));
+  try {
+    await schema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+    next();
+  } catch (err) {
+    const error = createHttpError(400, 'Bad Request', {
+      errors: err.details,
+    });
+    next(error);
+  }
 };
