6fc0985ee23f6987a45ef269493993786ef22595
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-14 23:02:19 +0300

    s@x&fix@rocknroll with validation

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,23 @@
+// import { isValidObjectId } from 'mongoose';
+// import createHttpError from 'http-errors';
+
+// export const isValidId = (req, res, next) => {
+//   const { contactId } = req.params;
+//   if (!isValidObjectId(contactId)) {
+//     throw createHttpError(400, 'Bad Request');
+//   }
+//   next();
+// };
+
 import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
+
   if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, 'Bad Request');
+    return next(createHttpError(400, 'Invalid contact ID'));
   }
+
   next();
 };
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,41 +1,26 @@
-// src/validation/сontacts.js
-
 import Joi from 'joi';
 
 export const createContactsSchema = Joi.object({
-  name: Joi.string()
-    .min(3)
-    .max(20)
-    .required()
-    .pattern(/^[a-zA-Z0-9@.-_]+$/)
-    .messages({
-      'string.base': 'Username should be a string',
-      'string.min': 'Username should have at least {#limit} characters',
-      'string.max': 'Username should have at most {#limit} characters',
-      'any.required': 'Username is required',
-    }),
-  phoneNumber: Joi.string()
-    .regex(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .required()
-    .messages({
-      'string.base': 'Phone number must be a string',
-      'string.pattern.base':
-        'Phone number must be in the format "(XXX) XXX-XXXX"',
-      'any.required': 'Phone number is required',
-    }),
-  email: Joi.string()
-    .email()
-    .optional()
-    .pattern(/^[a-zA-Z0-9@.-_]+$/)
-    .messages({
-      'string.email': 'Email must be a valid email address',
-      'string.base': 'Email should be a string',
-    }),
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().required().messages({
+    'string.base': 'phone number should be a string',
+    'any.required': 'Phone number is required',
+  }),
+  email: Joi.string().email().optional().messages({
+    'string.email': 'Email must be a valid email address',
+    'string.base': 'Email should be a string',
+  }),
   isFavourite: Joi.boolean().optional().messages({
     'boolean.base': 'isFavourite must be a boolean',
   }),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
+    .default('personal')
     .optional()
     .messages({
       'string.base': 'Contact type should be a string',
@@ -49,4 +34,4 @@ export const updateContactsSchema = Joi.object({
   email: Joi.string().email().optional(),
   isFavourite: Joi.boolean().optional(),
   contactType: Joi.string().valid('work', 'home', 'personal').optional(),
-});
+}).min(1);
