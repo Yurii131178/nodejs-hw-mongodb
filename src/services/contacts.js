9ab682826ad73c49dd6d89c14d2102d667c091b6
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-15 17:11:29 +0300

    change perpage to test render

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,7 +4,7 @@ import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
 export const getAllContacts = async ({
   page = 1,
-  perPage = 10,
+  perPage = 3,
   sortOrder = SORT_ORDER.ASC,
   sortBy = 'name',
   filter = {},
