1551478184b5cf8fa159e1302fcac7fbdbb7137a
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-30 05:06:33 +0300

    fix the refresh validity term

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -41,11 +41,11 @@ export const loginUserController = async (req, res) => {
 const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
+    expires: new Date(Date.now() + THIRTY_DAYS),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
+    expires: new Date(Date.now() + THIRTY_DAYS),
   });
 };
 
