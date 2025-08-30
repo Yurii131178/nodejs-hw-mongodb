18c75b11c7187f406237788d9bb46bcbcb1ff5d3
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-30 03:25:07 +0300

    add toJson to return objest without password _ update error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,19 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  res.status(err.status || 500).json({
-    status: err.status || 500,
-    message: err.message || 'Internal Server Error',
-    errors: err.errors || null,
+  // Перевірка, чи отримали ми помилку від createHttpError
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
   });
 };
