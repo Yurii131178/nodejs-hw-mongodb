e6ebb4da80ab8a69c6cf304a22a3cd416f1bebfb
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-30 13:38:53 +0300

    update validateBody to get detailed error message

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,14 +1,15 @@
 import createHttpError from 'http-errors';
 
-export const validateBody = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body, { abortEarly: false });
-  if (error) {
-    const errors = error.details.reduce((acc, d) => {
-      acc[d.context.key] = d.message;
-      return acc;
-    }, {});
-
-    return next(createHttpError(400, 'Bad Request', { errors }));
+export const validateBody = (schema) => async (req, res, next) => {
+  try {
+    await schema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+    next();
+  } catch (err) {
+    const error = createHttpError(400, 'Bad Request', {
+      errors: err.details,
+    });
+    next(error);
   }
-  next();
 };
