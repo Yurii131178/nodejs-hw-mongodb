2c36882e2f3959a063be36dea2d9aead9284581a
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-09-03 12:15:29 +0300

    add reset-password route

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { loginUserSchema, registerUserSchema, requestResetEmailSchema } from '../validation/auth.js';
-import { loginUserController, logoutUserController, refreshUserSessionController, registerUserController, requestResetEmailController } from '../controllers/auth.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/auth.js';
+import {
+  loginUserController,
+  logoutUserController,
+  refreshUserSessionController,
+  registerUserController,
+  requestResetEmailController,
+  resetPasswordController,
+} from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
@@ -22,11 +34,9 @@ router.post(
   ctrlWrapper(loginUserController),
 );
 
-
 // -----refresh------
 
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
-
 
 //---------logout---------
 
@@ -40,4 +50,10 @@ router.post(
   '/request-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController),
+);
+
+router.post(
+  '/reset-password',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
 );
