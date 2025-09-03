8efc3483058da270427756f4932c28a17924ec2e
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-09-03 12:05:53 +0300

    add reset password service and delete former session after pwd update

--- src/services/auth.js
+++ src/services/auth.js
@@ -157,3 +157,33 @@ export const requestResetToken = async (email) => {
     );
   }
 };
+
+// -------reset password--------
+
+export const resetPassword = async (payload) => {
+  let entries;
+
+  try {
+    entries = jwt.verify(payload.token, getEnvVar('JWT_SECRET'));
+  } catch {
+    throw createHttpError(401, 'Token is expired or invalid.');
+  }
+
+  const user = await UsersCollection.findOne({
+    email: entries.email,
+    _id: entries.sub,
+  });
+
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  await UsersCollection.updateOne(
+    { _id: user._id },
+    { password: encryptedPassword },
+  );
+
+  await SessionsCollection.deleteOne({ userId: user._id });
+};
