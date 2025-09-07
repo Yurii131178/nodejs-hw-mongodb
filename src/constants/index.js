b718e4578fed2e4e3a4fcb8b5fb4ab6cafb7a52f
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-09-07 08:22:21 +0300

    add swagger path to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -36,3 +36,7 @@ export const CLOUDINARY = {
   API_KEY: 'API_KEY',
   API_SECRET: 'API_SECRET',
 };
+
+// swagger path
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
